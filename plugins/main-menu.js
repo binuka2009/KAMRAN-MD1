3fd760a79acdd054a13e4e679b7ec2cd9b8cccaa
DRKAMRAN-MD <dkamran403@gmail.com>
2025-08-10 16:47:15 +0200

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -346,14 +346,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/7zfdcq.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/m1jjwx.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
+                        newsletterJid: '120363418144382782@newsletter',
                         newsletterName: config.BOT_NAME,
                         serverMessageId: 143
                     }
